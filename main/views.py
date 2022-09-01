e3896fad13b4f93168f92b01eddccd026948f2bd
madkingaerys <mariopranjic01@gmail.com>
2022-09-01 23:33:08 +0200

    added messages and navbar authenticated logic

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,12 +21,13 @@ def register(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
+            messages.success(request, f"New account has been created: {username}")
             login(request, user)
 
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
             return render(request=request,
                           template_name="main/register.html",
