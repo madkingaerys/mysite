68fe7a44ee01cfd36753e0f42af39318f77e04cd
madkingaerys <mariopranjic01@gmail.com>
2022-09-02 19:21:47 +0200

    Database changes and added tutorial categories and series

--- main/views.py
+++ main/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from main.templates.main.forms import NewUserForm
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def homepage(request):
     return render(request=request,
-                  template_name="main/home.html",
-                  context={"tutorials": Tutorial.objects.all})
+                  template_name="main/categories.html",
+                  context={"categories": TutorialCategory.objects.all})
 
 
 def register(request):
@@ -70,3 +71,22 @@ def login_request(request):
     return render(request=request,
                   template_name="main/login.html",
                   context={"form": form})
+
+
+def single_slug(request, single_slug):
+    # first check to see if the url is in categories.
+
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+        series_urls = {}
+
+        for m in matching_series.all():
+            part_one = Tutorial.objects\
+                .filter(tutorial_series__tutorial_series=m.tutorial_series)\
+                .earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request=request,
+                      template_name='main/category.html',
+                      context={"tutorial_series": matching_series, "part_ones": series_urls})
